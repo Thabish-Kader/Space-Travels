dc6390d91c300b185cb3c886f1a71b6fcf90f491
Thabish <kadertabish2@gmail.com>
2022-11-02 13:14:17 +0400

    authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Navbar } from "../components/Navbar";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+	Component,
+	pageProps,
+}: AppProps<{
+	session: Session;
+}>) {
 	return (
 		<>
-			<Navbar />
-			<Component {...pageProps} />
+			<SessionProvider session={pageProps.session}>
+				<Navbar />
+				<Component {...pageProps} />
+			</SessionProvider>
 		</>
 	);
 }
