54d7e785b5d0a07e45d9644b2a16d65952ee4766
Thabish <kadertabish2@gmail.com>
2022-11-02 21:42:49 +0400

    next auth and prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions = {
-	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
